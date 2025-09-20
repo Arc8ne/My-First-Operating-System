f103307f6875c88acdc078e64a24d2ace62469ef
Arc8ne <naveed.fazlan@gmail.com>
2025-09-20 12:03:09 +0800

    Update.

--- dbg.lua
+++ dbg.lua
@@ -3,18 +3,9 @@ local utils = require("lua/utils")
 
 local run = utils.run
 
-local lfs = require("lfs")
-
 local host_os_id = utils.get_host_os_id()
 
-bochs_cfg_file_path = lfs.currentdir() .. "/bochs-cfgs/" .. host_os_id:sub(1, 1):lower() .. host_os_id:sub(2)
-
-bochs_cfg_file, error_msg, error_code = io.open(bochs_cfg_file_path, "r")
-if bochs_cfg_file == nil then
-  error("Unable to find the corresponding Bochs configuration file for the current host OS.\nError message: " .. error_msg .. "\nError code: " .. error_code)
-  return 1
-end
-bochs_cfg_file:close()
+bochs_cfg_file_path = "bochs-cfgs/" .. host_os_id:sub(1, 1):lower() .. host_os_id:sub(2)
 
 local bochs_cmd = "bochs -q -f " .. bochs_cfg_file_path
 
