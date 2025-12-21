7565d109bc6ff46a3aa559189d85f342ee3b0b9b
Arc8ne <naveed.fazlan@gmail.com>
2025-12-21 14:36:51 +0800

    Update build script to ensure that it works regardless of the size of the bootloader's 2nd stage.

--- build.lua
+++ build.lua
@@ -17,8 +17,40 @@ local get_num_bytes_in_file = function(file_path)
   file:close()
   return num_bytes_in_file
 end
--- Build the kernel using `clang` as it supports cross-compilation out of the box.
-exit_if_fail("clang -m32 -ffreestanding -fno-builtin -nostdlib -fno-PIC -T kernel.ld -o bin/kernel.bin src/kernel/*.c")
+local compile_kernel = function(kernel_starting_memory_address)
+  local kernel_compile_cmd = "clang -m32 -ffreestanding -fno-builtin -nostdlib -fno-PIC -o bin/kernel.bin src/kernel/*.c"
+  local kernel_linker_script = string.format([[
+OUTPUT_FORMAT("binary")
+SECTIONS
+{
+  . = 0x%x;
+  .text :
+  {
+    *(.text.kernel_main)
+    *(.text)
+  }
+  .data :
+  {
+    *(.data)
+  }
+  .bss :
+  {
+    *(.bss)
+  }
+}
+]], kernel_starting_memory_address)
+  -- TODO: Add support for Windows.
+  exit_if_fail(
+    string.format(
+      "bash -c '%s -T <(echo \"%s\")'",
+      kernel_compile_cmd,
+      kernel_linker_script
+    )
+  )
+end
+-- Compile the kernel with a placeholder starting memory address specified in the generated linker script so that the bootloader's 2nd stage can still have access to the actual size of the kernel binary.
+-- Note: `clang` is used as the compiler as it supports cross-compilation out of the box.
+compile_kernel(0x800a)
 -- Build the 2nd stage (i.e. post-MBR) of the bootloader.
 local num_bytes_per_sector = 512
 local kernel_binary_size_in_bytes = get_num_bytes_in_file("bin/kernel.bin")
@@ -31,6 +63,11 @@ local bootloader_stage_2_binary_size_in_sectors = math.ceil(
   bootloader_stage_2_binary_size_in_bytes / num_bytes_per_sector
 )
 exit_if_fail("truncate -s " .. bootloader_stage_2_binary_size_in_sectors * num_bytes_per_sector .. " bin/bootloader/2nd-stage.bin")
+-- Re-compile the kernel with its actual starting memory address specified in the generated linker script.
+local bootloader_stage_2_start_address = 0x7e00
+local kernel_unaligned_start_address = 0x7e00 + bootloader_stage_2_binary_size_in_bytes
+local kernel_aligned_start_address = kernel_unaligned_start_address + (16 - (kernel_unaligned_start_address % 16))
+compile_kernel(kernel_aligned_start_address)
 -- Build the 1st stage (i.e. MBR) of the bootloader.
 exit_if_fail("nasm -f bin -o bin/bootloader/1st-stage.bin -d BOOTLOADER_STAGE_2_SIZE_IN_SECTORS=" .. bootloader_stage_2_binary_size_in_sectors .. " src/bootloader/1st-stage/main.asm")
 -- Create a floppy disk image.
